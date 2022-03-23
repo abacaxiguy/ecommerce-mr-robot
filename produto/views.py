75ad7057d150622e726a39ce7eaf3412b76fdacc
João Lucas <Jlucasoc@hotmail.com>
2022-03-23 17:53:30 -0300

    preparing for new readme 🎉

--- produto/views.py
+++ produto/views.py
@@ -14,6 +14,7 @@ class ProductList(ListView):
     template_name = 'produto/list.html'
     context_object_name = 'products'
     paginate_by = 6
+    ordering = ['-id']
 
 
 class Search(ProductList):
